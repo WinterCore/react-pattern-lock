af34793802e5494d558f0e4f8c8e64d9826a75ba
WinterCore <hogobbl@gmail.com>
2018-11-06 21:22:14 +0200

    Fixed wrong output library name

--- webpack.config.js
+++ webpack.config.js
@@ -32,7 +32,7 @@ module.exports = {
 	output : {
 		path          : path.join(__dirname, "dist"),
 		filename      : "index.js",
-		library       : "TextTransition",
+		library       : "ReactPatternLock",
 		libraryTarget : "umd"
 	}
 };
