1e37945addd268715ad54074dff857199142d36b
Toufik <said.toufik.simplon@gmail.com>
2023-11-29 00:57:53 +0100

    Added ability to choose first point color through props, updated all packages, and updated to the latest React version

--- demos/src/Components/Demo.tsx
+++ demos/src/Components/Demo.tsx
@@ -3,22 +3,22 @@ import * as React from "react";
 import PatternLock from "../../../src/index";
 
 type IDemoState = {
-    path      : number[],
-    isLoading : boolean,
-    error     : boolean,
-    success   : boolean,
-    disabled  : boolean,
-    size      : number,
+	path: number[],
+	isLoading: boolean,
+	error: boolean,
+	success: boolean,
+	disabled: boolean,
+	size: number,
 }
 
 class Demo extends React.Component<{}, IDemoState> {
 	state = {
-		path      : [] as number[],
-		isLoading : false,
-		error     : false,
-		success   : false,
-		disabled  : false,
-		size      : 3
+		path: [] as number[],
+		isLoading: false,
+		error: false,
+		success: false,
+		disabled: false,
+		size: 3
 	};
 
 	errorTimeout: number = 0;
@@ -28,41 +28,41 @@ class Demo extends React.Component<{}, IDemoState> {
 			const { key } = e;
 			if (key === "ArrowUp") {
 				e.preventDefault();
-				this.setState({ size : this.state.size >= 10 ? 10 : this.state.size + 1 });
+				this.setState({ size: this.state.size >= 10 ? 10 : this.state.size + 1 });
 			} else if (key === "ArrowDown") {
 				e.preventDefault();
-				this.setState({ size : this.state.size > 3 ? this.state.size - 1 : 3 });
+				this.setState({ size: this.state.size > 3 ? this.state.size - 1 : 3 });
 			}
 		});
 	}
 
 	onReset = () => {
 		this.setState({
-			path     : [],
-			success  : false,
-			error    : false,
-			disabled : false
+			path: [],
+			success: false,
+			error: false,
+			disabled: false
 		});
 	};
 
 	onChange = (path: number[]) => {
-		this.setState({ path : [...path] });
+		this.setState({ path: [...path] });
 	};
 
 	onFinish = () => {
-		this.setState({ isLoading : true });
+		this.setState({ isLoading: true });
 		// an imaginary api call
 		setTimeout(() => {
 			if (this.state.path.join("-") === "0-1-2") {
-				this.setState({ isLoading : false, success : true, disabled : true });
+				this.setState({ isLoading: false, success: true, disabled: true });
 			} else {
-				this.setState({ disabled : true, error : true });
+				this.setState({ disabled: true, error: true });
 				this.errorTimeout = window.setTimeout(() => {
 					this.setState({
-						disabled  : false,
-						error     : false,
-						isLoading : false,
-						path      : []
+						disabled: false,
+						error: false,
+						isLoading: false,
+						path: []
 					});
 				}, 3000);
 			}
@@ -76,24 +76,26 @@ class Demo extends React.Component<{}, IDemoState> {
 			<React.Fragment>
 				<div className="center">
 					<PatternLock
-						size={ size }
-						onChange={ this.onChange }
-						path={ path }
-						error={ error }
-						onFinish={ this.onFinish }
-						connectorThickness={ 5 }
-						disabled={ disabled || isLoading }
-						success={ success }
+						size={size}
+						onChange={this.onChange}
+						path={path}
+						error={error}
+						onFinish={this.onFinish}
+						connectorThickness={5}
+						disabled={disabled || isLoading}
+						success={success}
+						firstPointColorEnabled={true}
+						firstPointColor={"green"}
 					/>
 				</div>
 				<div className="output">
 					Select the top 3 points starting from the left
 				</div>
 				<div className="output">
-					Output : { this.state.path.join(", ") }
+					Output : {this.state.path.join(", ")}
 				</div>
 				{
-					success && <button style={{ margin : "0 auto", display : "block" }} onClick={ this.onReset }>Click here to reset</button>
+					success && <button style={{ margin: "0 auto", display: "block" }} onClick={this.onReset}>Click here to reset</button>
 				}
 				<div className="output">
 					Press the up/down arrow keys to increase/decrease the size of the input
--- src/components/Point.tsx
+++ src/components/Point.tsx
@@ -1,12 +1,14 @@
 import * as React from "react";
 
 interface PointProps {
-    index           : number;
-    pointSize       : number;
-    pointActiveSize : number;
-    size            : number;
-    pop             : boolean;
-    selected        : boolean;
+    index: number;
+    pointSize: number;
+    pointActiveSize: number;
+    size: number;
+    pop: boolean;
+    selected: boolean;
+    firstPointColorEnabled: boolean;
+    firstPointColor: string;
 }
 
 const Point: React.FunctionComponent<PointProps> = ({
@@ -15,38 +17,41 @@ const Point: React.FunctionComponent<PointProps> = ({
     pointActiveSize,
     size,
     selected,
-    pop
+    pop,
+    firstPointColorEnabled,
+    firstPointColor
 }) => {
     const percentPerItem = 100 / size;
+    const pointInnerStyle = (index === 0 && firstPointColorEnabled) ? { backgroundColor: firstPointColor } : {};
 
     return (
         <div
-            className={ `react-pattern-lock__point-wrapper${selected ? " selected" : ""}` }
+            className={`react-pattern-lock__point-wrapper${selected ? " selected" : ""}`}
             style={{
-                width  : `${percentPerItem}%`,
-                height : `${percentPerItem}%`,
-                flex   : `1 0 ${percentPerItem}%`
+                width: `${percentPerItem}%`,
+                height: `${percentPerItem}%`,
+                flex: `1 0 ${percentPerItem}%`
             }}
-            data-index={ index }
+            data-index={index}
         >
             <div
                 className="react-pattern-lock__point"
                 style={{
-                    width  : pointActiveSize,
-                    height : pointActiveSize
+                    width: pointActiveSize,
+                    height: pointActiveSize
                 }}
             >
                 <div
-                    className={ `react-pattern-lock__point-inner${pop ? " active" : ""}` }
+                    className={`react-pattern-lock__point-inner${pop ? " active" : ""}`}
                     style={{
-                        minWidth   : pointSize,
-                        minHeight  : pointSize
+                        minWidth: pointSize,
+                        minHeight: pointSize,
+                        ...pointInnerStyle // Appliquer le style conditionnel au point lui-même
                     }}
                 />
             </div>
         </div>
     );
 };
-
 
 export default Point;
